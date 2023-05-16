4bcc6d2639b1085beb2d6a0720e3e8f2029b9890
pawsob89 <pawelkamil.sobon@gmail.com>
2023-05-16 20:35:31 +0200

    poprawiony plik script.js

--- Projekt DEV - Zakup auta (Pawel)/script.js
+++ Projekt DEV - Zakup auta (Pawel)/script.js
@@ -1,19 +1,19 @@
 //linia kodu spowalniająca animacje na stronie
 document.getElementById("backgroundVid").playbackRate = 0.5;
 
-//funkcja przeliczająca datę od dnia bieżącego + 14 dni, przekazuje wartość do HTML (wartość min) którą uniemożliwia
-//wybranie daty wcześniejszej za pomocą kaledarza
+//funkcja przeliczająca datę od dnia bieżącego + 14 dni, przekazuje wartość do HTML (wartość min) którą uniemożliwia 
+//wybranie daty wcześniejszej za pomocą kaledarza 
 let currentDate = new Date(Date.now() + 12096e5);
 let currentDay, currentMonth, currentYear;
 currentDay = currentDate.getDate();
-currentMonth = currentDate.getMonth() + 1;
+currentMonth = currentDate.getMonth()+1;
 currentYear = currentDate.getFullYear();
-let $myDate = `${currentYear}-0${currentMonth}-0${currentDay}`;
-let $newDate = $myDate;
-document.getElementById("datePicker").min = $myDate;
-document.getElementById("datePicker").value = $myDate;
+let myDate = `${currentYear}-0${currentMonth}-${currentDay}`;
+let newDate = myDate;
+document.getElementById("datePicker").min = myDate;
+document.getElementById("datePicker").value = myDate;
 
-//kod w którym wykonuje się działanie po wybraniu pola z samochodem Porshe na stronie głównej
+//pobranie elementów HTML po id i przypisanie ich do stałych
 const $Porsche = document.getElementById("Porsche");
 const $Audi = document.getElementById("Audi");
 const $BMW = document.getElementById("BMW");
@@ -21,12 +21,12 @@ const $Mercedes = document.getElementById("Mercedes");
 const $Jaguar = document.getElementById("Jaguar");
 const $Volvo = document.getElementById("Volvo");
 
-//definicja zmiennych zapisujących informacje w localStorage
-let $userString;
-let $userInLocalStorage;
+//definicja zmiennych użytkownika zapisujących informacje w localStorage
+let userString;
+let userInLocalStorage;
 //definicja uzytkownika zapisanego w localStorage
 
-let $storedUser = {
+let storedUser = {
   firstName: "",
   lastName: "",
   street: "",
@@ -39,7 +39,7 @@ let $storedUser = {
 };
 
 //definicja użytkownika i jego elementów w formie obiektu
-let $user = {
+let user = {
   firstName: "",
   lastName: "",
   street: "",
@@ -52,26 +52,26 @@ let $user = {
 };
 // Wyświetlenie informacji zapisanych w storedUser w formularzu z warunkiem wykonania jeśli są zapisane
 
-$userInLocalStorage = localStorage.getItem($storedUser); //wczytuje z local storage string
-$storedUser = JSON.parse($userInLocalStorage); // zamiana na tablice po zapisaniu w local storage
+userInLocalStorage = localStorage.getItem(storedUser); //wczytuje z local storage string
+storedUser = JSON.parse(userInLocalStorage); // zamiana na tablice po zapisaniu w local storage
 
-//funkcja która pobiera informacje o użytkowniku z localStorage i przypisauje je do odpowiednich pól w formularzu
-function LocStorage() {
-  $userInLocalStorage = localStorage.getItem($storedUser); //wczytuje z local storage string
-  $storedUser = JSON.parse($userInLocalStorage); // zamiana na tablice po zapisaniu w local storage
+//funkcja która pobiera informacje o użytkowniku z localStorage i przypisauje je do odpowiednich pól w formularzu 
+function locStorage() {
+  userInLocalStorage = localStorage.getItem(storedUser); //wczytuje z local storage string
+  storedUser = JSON.parse(userInLocalStorage); // zamiana na tablice po zapisaniu w local storage
 
-  if ($storedUser === null) {
+  if (storedUser === null) {
     console.log("brak usera w localStorage");
   } else {
-    document.getElementById("firstName").value = $storedUser.firstName;
-    document.getElementById("lastName").value = $storedUser.lastName;
-    document.getElementById("street").value = $storedUser.street;
-    document.getElementById("number").value = $storedUser.number;
-    document.getElementById("zipCode").value = $storedUser.zipCode;
-    document.getElementById("city").value = $storedUser.city;
-    document.getElementById("email").value = $storedUser.email;
-    document.getElementById("phone").value = $storedUser.phone;
-    document.getElementById("datePicker").value = $storedUser.date;
+    document.getElementById("firstName").value = storedUser.firstName;
+    document.getElementById("lastName").value = storedUser.lastName;
+    document.getElementById("street").value = storedUser.street;
+    document.getElementById("number").value = storedUser.number;
+    document.getElementById("zipCode").value = storedUser.zipCode;
+    document.getElementById("city").value = storedUser.city;
+    document.getElementById("email").value = storedUser.email;
+    document.getElementById("phone").value = storedUser.phone;
+    document.getElementById("datePicker").value = storedUser.date
   }
 }
 
@@ -81,83 +81,28 @@ let $carPrice = 0;
 let $optionPrice = 0;
 let $finalPrice = 0;
 
-$Porsche.addEventListener("click", () => {
-  const sectionCars = document.querySelector(".sectionCars");
-  sectionCars.style.display = "none";
-  const sectionFormContainer = document.querySelector(".formContainer");
-  sectionFormContainer.style.display = "flex";
-  document.querySelector(".returnButton").style.display = "block";
+// funkcja która podczas wyboru auta zmienia widok strony oraz zapisuje w localstorage
+function carPicker(car, desc) {
+  let queryItem = `#`+ desc+ ` #carPrice`;
+  car.addEventListener("click", () => {
+    const sectionCars = document.querySelector(".sectionCars");
+    sectionCars.style.display = "none";
+    const sectionFormContainer = document.querySelector(".formContainer");
+    sectionFormContainer.style.display = "flex";
+    document.querySelector(".returnButton").style.display = "block";
+    $carPrice = document.querySelector(queryItem).innerText;
+    document.getElementById("selectedCar").innerHTML = document.getElementById(desc).innerHTML;
+    locStorage();
+  });
+}
 
-  $carPrice = document.querySelector("#Porsche #carPrice").innerText;
-  document.getElementById("selectedCar").innerHTML =
-    document.getElementById("Porsche").innerHTML;
-  LocStorage();
-});
-
-$Audi.addEventListener("click", () => {
-  const sectionCars = document.querySelector(".sectionCars");
-  sectionCars.style.display = "none";
-  const sectionFormContainer = document.querySelector(".formContainer");
-  sectionFormContainer.style.display = "flex";
-  document.querySelector(".returnButton").style.display = "block";
-
-  $carPrice = document.querySelector("#Audi #carPrice").innerText;
-  document.getElementById("selectedCar").innerHTML =
-    document.getElementById("Audi").innerHTML;
-  LocStorage();
-});
-
-$BMW.addEventListener("click", () => {
-  const sectionCars = document.querySelector(".sectionCars");
-  sectionCars.style.display = "none";
-  const sectionFormContainer = document.querySelector(".formContainer");
-  sectionFormContainer.style.display = "flex";
-  document.querySelector(".returnButton").style.display = "block";
-
-  $carPrice = document.querySelector("#BMW #carPrice").innerText;
-  document.getElementById("selectedCar").innerHTML =
-    document.getElementById("BMW").innerHTML;
-  LocStorage();
-});
-
-$Mercedes.addEventListener("click", () => {
-  const sectionCars = document.querySelector(".sectionCars");
-  sectionCars.style.display = "none";
-  const sectionFormContainer = document.querySelector(".formContainer");
-  sectionFormContainer.style.display = "flex";
-  document.querySelector(".returnButton").style.display = "block";
-
-  $carPrice = document.querySelector("#Mercedes #carPrice").innerText;
-  document.getElementById("selectedCar").innerHTML =
-    document.getElementById("Mercedes").innerHTML;
-  LocStorage();
-});
-
-$Jaguar.addEventListener("click", () => {
-  const sectionCars = document.querySelector(".sectionCars");
-  sectionCars.style.display = "none";
-  const sectionFormContainer = document.querySelector(".formContainer");
-  sectionFormContainer.style.display = "flex";
-  document.querySelector(".returnButton").style.display = "block";
-
-  $carPrice = document.querySelector("#Jaguar #carPrice").innerText;
-  document.getElementById("selectedCar").innerHTML =
-    document.getElementById("Jaguar").innerHTML;
-  LocStorage();
-});
-
-$Volvo.addEventListener("click", () => {
-  const sectionCars = document.querySelector(".sectionCars");
-  sectionCars.style.display = "none";
-  const sectionFormContainer = document.querySelector(".formContainer");
-  sectionFormContainer.style.display = "flex";
-  document.querySelector(".returnButton").style.display = "block";
-
-  $carPrice = document.querySelector("#Volvo #carPrice").innerText;
-  document.getElementById("selectedCar").innerHTML =
-    document.getElementById("Volvo").innerHTML;
-  LocStorage();
-});
+//wywołanie funkcji nasłuchiwania eventu wyboru auta i przejście do kolejnego widoku strony 
+carPicker($Porsche, "Porsche");
+carPicker($Audi, "Audi");
+carPicker($BMW, "BMW");
+carPicker($Mercedes, "Mercedes");
+carPicker($Jaguar, "Jaguar");
+carPicker($Volvo, "Volvo");
 
 //działanie przycisku powrót do wyboru aut
 const $returnBtn = document.getElementById("returnBtn");
@@ -167,34 +112,34 @@ $returnBtn.addEventListener("click", () => {
   document.querySelector(".returnButton").style.display = "none";
 });
 
-let $payMethod = "Gotówka";
+let payMethod = "Gotówka";
 let $payMethodRadio = document.querySelector("#cash");
 let $OCinsuranceCheck = document.getElementById("OCinsurance");
 let $ACinsuranceCheck = document.getElementById("ACinsurance");
 let $ceramicCoatCheck = document.getElementById("ceramicCoat");
 
 $optionBtn.addEventListener("click", () => {
-  let $OCinsurancePrice = 0;
-  let $ACinsurancePrice = 0;
-  let $ceramicCoatPrice = 0;
+  let OCinsurancePrice = 0;
+  let ACinsurancePrice = 0;
+  let ceramicCoatPrice = 0;
 
   if ($OCinsuranceCheck.checked) {
-    $OCinsurancePrice = 4.6;
+    OCinsurancePrice = 4.6;
   } else {
-    $OCinsurancePrice = 0;
+    OCinsurancePrice = 0;
   }
   if ($ACinsuranceCheck.checked) {
-    $ACinsurancePrice = 17.5;
+    ACinsurancePrice = 17.5;
   } else {
-    $ACinsurancePrice = 0;
+    ACinsurancePrice = 0;
   }
   if ($ceramicCoatCheck.checked) {
-    $ceramicCoatPrice = 6;
+    ceramicCoatPrice = 6;
   } else {
-    $ceramicCoatPrice = 0;
+    ceramicCoatPrice = 0;
   }
 
-  $optionPrice = +$OCinsurancePrice + $ACinsurancePrice + $ceramicCoatPrice;
+  $optionPrice = +OCinsurancePrice + ACinsurancePrice + ceramicCoatPrice;
   $carPrice = document.querySelector("#selectedCar #carPrice").innerText;
   $finalPrice = +$carPrice + $optionPrice;
 
@@ -206,35 +151,35 @@ $optionBtn.addEventListener("click", () => {
 $summaryBtn.addEventListener("click", () => {
   //zapisywanie informacji o metodzie płatności oraz zapisywanie wartości auta i opcji
   if ($payMethodRadio.checked) {
-    $payMethod = "Gotówka";
+    payMethod = "Gotówka";
   } else {
-    $payMethod = "Leasing";
+    payMethod = "Leasing";
   }
   $carPrice = document.querySelector("#selectedCar #carPrice").innerText;
   $optionPrice = document.getElementById("optionsPrice").innerText;
   $finalPrice = document.getElementById("finalPrice").innerText;
-  //pobieranie wartości z formularza i przypisywanie ich do odpowiednich kluczy w obiekcie $user
-  $user.firstName = document.getElementById("firstName").value;
-  $user.lastName = document.getElementById("lastName").value;
-  $user.street = document.getElementById("street").value;
-  $user.number = document.getElementById("number").value;
-  $user.zipCode = document.getElementById("zipCode").value;
-  $user.city = document.getElementById("city").value;
-  $user.email = document.getElementById("email").value;
-  $user.phone = document.getElementById("phone").value;
-  $user.date = document.getElementById("datePicker").value;
-  let $validate;
+  //pobieranie wartości z formularza i przypisywanie ich do odpowiednich kluczy w obiekcie user
+  user.firstName = document.getElementById("firstName").value;
+  user.lastName = document.getElementById("lastName").value;
+  user.street = document.getElementById("street").value;
+  user.number = document.getElementById("number").value;
+  user.zipCode = document.getElementById("zipCode").value;
+  user.city = document.getElementById("city").value;
+  user.email = document.getElementById("email").value;
+  user.phone = document.getElementById("phone").value;
+  user.date = document.getElementById("datePicker").value;
+  let validate;
 
-  console.log($user);
-  for (let [key, value] of Object.entries($user)) {
+  console.log(user);
+  for (let [key, value] of Object.entries(user)) {
     if (`${value}` === "") {
-      $validate = true;
+      validate = true;
     }
   }
   //stała pobierajaca stylowanie dla klasy.warning
   const warningMes = document.querySelector(".warning");
   //warunek kiedy jakiekolwiek pole jest puste to pojawi się ostrzeżenie
-  if ($validate === true) {
+  if (validate === true) {
     //pojawienie sie informacji o niewypełnionym w pełni formularzu z warunkiem (walidacja)
     warningMes.style.visibility = "visible";
   } else {
@@ -244,32 +189,31 @@ $summaryBtn.addEventListener("click", () => {
     document.querySelector(".summaryContainer").style.display = "flex";
     document.querySelector(".returnButton").style.display = "none";
     document.querySelector(".clearButton").style.display = "block";
-    document.getElementById("mainText").innerText =
-      "Dziękujemy za zakup! oto podsumowanie!";
+    document.getElementById("mainText").innerText = "Dziękujemy za zakup! oto podsumowanie!"
 
-    $storedUser;
-    $userString = JSON.stringify($user); //zamieniam tablice z autem na string przed zapisaniem w local storage
-    localStorage.setItem($storedUser, $userString); //zapisuje w local storage string
-    document.getElementById("summaryCar").innerHTML =
-      document.getElementById("selectedCar").innerHTML;
-    document.getElementById("payMethod").innerText = $payMethod;
+    storedUser;
+    userString = JSON.stringify(user); //zamieniam tablice z userem na string przed zapisaniem w local storage
+    localStorage.setItem(storedUser, userString); //zapisuje w local storage string
+    document.getElementById("summaryCar").innerHTML = document.getElementById("selectedCar").innerHTML
+    document.getElementById("payMethod").innerText = payMethod;
     document.getElementById("sumOptionsPrice").innerText = $optionPrice;
     document.getElementById("sumFinalPrice").innerText = $finalPrice;
 
-    document.getElementById("sumFName").innerText = $user.firstName;
-    document.getElementById("sumLName").innerText = $user.lastName;
-    document.getElementById("sumStreet").innerText = $user.street;
-    document.getElementById("sumNumber").innerText = $user.number;
-    document.getElementById("sumZipCode").innerText = $user.zipCode;
-    document.getElementById("sumCity").innerText = $user.city;
-    document.getElementById("sumEmail").innerText = $user.email;
-    document.getElementById("sumPhone").innerText = $user.phone;
-    document.getElementById("sumDate").innerText = $user.date;
+    document.getElementById("sumFName").innerText = user.firstName;
+    document.getElementById("sumLName").innerText = user.lastName;
+    document.getElementById("sumStreet").innerText = user.street;
+    document.getElementById("sumNumber").innerText = user.number;
+    document.getElementById("sumZipCode").innerText = user.zipCode; 
+    document.getElementById("sumCity").innerText = user.city;
+    document.getElementById("sumEmail").innerText = user.email;
+    document.getElementById("sumPhone").innerText = user.phone;
+    document.getElementById("sumDate").innerText = user.date;
   }
-});
 
+});
+// funkcja czyszcząca localStorage i przenosi użytkownika do strony głównej 
 const $clearBtn = document.getElementById("clearBtn");
 $clearBtn.addEventListener("click", () => {
-  window.localStorage.clear($storedUser);
+  window.localStorage.clear(storedUser);
   window.location.href = "index.html";
 });
